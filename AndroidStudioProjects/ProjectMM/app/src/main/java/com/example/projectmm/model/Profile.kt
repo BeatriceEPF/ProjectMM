355841ae26282d2975d91d2b5aa6c74a1cbc2751
BeatriceEPF <beatrice.garciacegarra@epfedu.fr>
2023-05-22 22:59:59 +0100

    Resolve Connection issue in Details movie Activity (Check connected state)

--- AndroidStudioProjects/ProjectMM/app/src/main/java/com/example/projectmm/model/Profile.kt
+++ AndroidStudioProjects/ProjectMM/app/src/main/java/com/example/projectmm/model/Profile.kt
@@ -13,23 +13,4 @@ data class Profile(
     val passwd: String,
     val favMoviesList: Array<Int>
 
-) : Parcelable {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Profile
-
-        if (id != other.id) return false
-        if (passwd != other.passwd) return false
-        if (!favMoviesList.contentEquals(other.favMoviesList)) return false
-
-        return true
-    }
-    override fun hashCode(): Int {
-        var result = id.hashCode()
-        result = 31 * result + passwd.hashCode()
-        result = 31 * result + favMoviesList.contentHashCode()
-        return result
-    }
-}
+) : Parcelable {}
